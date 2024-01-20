1a1faea92c6a078dcbe4458a2ff79c2cb30fe556
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-20 22:31:34 +0900

    Chapter3-3

--- src/sample/JavaSampleClass.java
+++ src/sample/JavaSampleClass.java
@@ -1,9 +1,18 @@
 package sample;
 
 public class JavaSampleClass {
-	public static void main(String []arg) {
-		for (int i = 0; i < 3; i++) {
-			System.out.print("現在" + (i + 1) + "週目->");
+	public static void main(String []args) {
+		int isHungry =1;
+		String food = "ラーメン";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹いっぱいです");
+		} else {
+			System.out.println("はらぺこです");
 		}
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+	System.out.println("ごちそうさまでした");
 	}
 }
