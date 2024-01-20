6a8c9a560db4d97f78c53bb4b101542f8ccb4342
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-20 23:02:40 +0900

    Chapter3-6

--- src/sample/JavaSampleClass.java
+++ src/sample/JavaSampleClass.java
@@ -2,17 +2,22 @@ package sample;
 
 public class JavaSampleClass {
 	public static void main(String []args) {
-		int isHungry =1;
-		String food = "ラーメン";
-		System.out.println("こんにちは");
-		if (isHungry == 0) {
-			System.out.println("お腹いっぱいです");
-		} else {
-			System.out.println("はらぺこです");
+		System.out.println("[数あてゲーム]");
+		//０〜９までの整数の中からランダムな数を１つ生成して変数ansに代入する
+		int ans = new java.util.Random().nextInt(10);
+		//forを用いた５回繰り返するループ
+		for (int i = 0; i < 5; i++) {
+			System.out.println("０〜９の数字を入力してください");
+			//数字を入力し変数numに代入する
+			int num = new java.util.Scanner(System.in).nextInt();
+			//もし変数numが変数ansと等しかったらアタリと表示され終了
+			if (ans == num) {
+				System.out.println("アタリ!");
+				break;
+			} else {
+				System.out.println("違います");
+			}
 		}
-		if (isHungry == 1) {
-			System.out.println(food + "をいただきます");
-		}
-	System.out.println("ごちそうさまでした");
+		System.out.println("ゲームを終了します");
 	}
 }
