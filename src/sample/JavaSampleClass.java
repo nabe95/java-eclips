528ae0bf87c744da31010da8fb59e63275e665d7
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-20 18:55:15 +0900

    for文練習

--- src/sample/JavaSampleClass.java
+++ src/sample/JavaSampleClass.java
@@ -1,19 +1,9 @@
 package sample;
 
 public class JavaSampleClass {
-
-	public static void main(String[] args) {
-		System.out.println("ようこそ占いの館へ");
-		System.out.print("あなたの名前を入力してください＞");
-		String name = new java.util.Scanner(System.in).nextLine();
-		System.out.print("あなたの年齢を入力してください＞");
-		String ageString = new java.util.Scanner(System.in).nextLine();
-		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(4);
-		fortune++;
-		System.out.println("占いの結果が出ました!");
-		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-		System.out.println("(1:大吉 2:中吉 3:吉 4:凶)");
+	public static void main(String []arg) {
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) + "週目->");
+		}
 	}
-
 }
