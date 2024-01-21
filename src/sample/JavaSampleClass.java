56593b1472bfcf4e9d45c34b66f264cd4a43e91d
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-21 14:30:31 +0900

    練習問題1

--- src/sample/JavaSampleClass.java
+++ src/sample/JavaSampleClass.java
@@ -3,7 +3,8 @@ package sample;
 public class JavaSampleClass {
 	//1〜10までの数字のうち偶数のみを昇順で表示させる
 	public static void main(String[] args) {
-		for ( int i =1; i <= 10; i++) {
+		System.out.println("偶数の昇順");
+		for (int i = 1; i <= 10; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i);
 			}
---
+++ src/sample/Practice1.java
@@ -0,0 +1,14 @@
+package sample;
+
+//1~10までの数字のうち偶数のみを昇順で表示する
+public class Practice1 {
+	public static void main(String[] args) {
+		System.out.println("偶数のみ昇順");
+		
+		for (int i= 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
