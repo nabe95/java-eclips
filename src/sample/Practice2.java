f09a29df43079ba9400b2cd35142756ee3252724
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-21 14:38:17 +0900

    練習問題3

---
+++ src/sample/Practice2.java
@@ -0,0 +1,18 @@
+package sample;
+
+//1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
+public class Practice2 {
+	public static void main(String []args) {
+		int count = 0;
+		int sum = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				count++;
+				sum += i;
+			}
+		}
+		System.out.println("偶数の個数" + count);
+		System.out.println("偶数の合計" + sum);
+	}
+}
