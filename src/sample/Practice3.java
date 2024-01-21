1308435ece7befbd8b67329ec20ca03296743656
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-21 14:49:57 +0900

    練習問題3

---
+++ src/sample/Practice3.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Practice3 {
+//	1〜10までの整数をカンマ区切りで横並びで表示させる（10にはカンマをつけない）
+	public static void main(String[] args) { 
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i);
+			if (i < 10) {
+				System.out.print(", ");
+			}
+		}
+	}
+}
