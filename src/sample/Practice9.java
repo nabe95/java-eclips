9cf6b75c7e3432738bb1792101a0dedfb0714208
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-29 01:41:30 +0900

    練習:配列

---
+++ src/sample/Practice9.java
@@ -0,0 +1,32 @@
+package sample;
+
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+public class Practice9 {
+	public static void main(String[] args) {
+		//要素数20の配列を宣言し
+		int[] num = new int[20];
+		for (int i = 1; i < num.length; i++) {
+			num[i] = i * 5;
+		}
+		//[0]~[10]の値の偶数のみカンマ区切り
+		System.out.println("[0]~[10]の値の奇数");
+		for (int i = 0; i <= 10; i++) {
+			if (num[i] % 2 == 0) {
+				System.out.print(num[i]);
+				if (i < 10) {
+					System.out.print(", ");
+				}
+			}
+		}
+		//[11]~[19]の値は奇数をカンマ区切り
+		System.out.println("\n[11]~[19]の偶数");
+		for (int i = 11; i < num.length; i++) {
+			if (num[i] % 2 != 0) {
+				System.out.print(num[i]);
+				if (i < 19) {
+					System.out.print(", ");
+				}
+			}
+		}
+	}
+}
