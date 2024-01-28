4a99e74c2abba954d3ef00b5058584b946d2916a
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-29 01:22:22 +0900

    練習:奇数

--- src/sample/Practice8.java
+++ src/sample/Practice8.java
@@ -1,10 +1,18 @@
 package sample;
 
+//1〜10までに奇数が何個あるか、また奇数の合計は幾つになるか表示させる
 public class Practice8 {
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 10;
-		int m = Math.max(a, b);
-		System.out.println("比較実験" + a + "と" + b + "で大きい方は" + m);
+		int count = 0;
+		int sum = 0;
+
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 != 0) {
+				count++;
+				sum += i;
+			}
+		}
+		System.out.println(count);
+		System.out.println(sum);
 	}
 }
