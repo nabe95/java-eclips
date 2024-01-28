943db0927589f7ada9bcf0834cd68be6922efc4a
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-29 00:01:11 +0900

    比較して大きい値

--- src/sample/Practice8.java
+++ src/sample/Practice8.java
@@ -2,9 +2,9 @@ package sample;
 
 public class Practice8 {
 	public static void main(String[] args) {
-		String name = "渡邉";
-		String message;
-		message = name + "さん、こんにちは";
-		System.out.println(message);
+		int a = 2;
+		int b = 10;
+		int m = Math.max(a, b);
+		System.out.println("比較実験" + a + "と" + b + "で大きい方は" + m);
 	}
 }
