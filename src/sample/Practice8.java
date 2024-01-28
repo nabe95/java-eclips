43115b6bc1c22ae6da7bf831fd5dc538a218f154
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-28 23:58:19 +0900

    命令実行文

---
+++ src/sample/Practice8.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Practice8 {
+	public static void main(String[] args) {
+		String name = "渡邉";
+		String message;
+		message = name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
