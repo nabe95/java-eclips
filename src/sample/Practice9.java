85b78c85ae8bb6faf9aa62b82b25e1f82c2b6b5d
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-01-30 02:07:41 +0900

    練習3

--- src/sample/Practice9.java
+++ src/sample/Practice9.java
@@ -1,6 +1,6 @@
 package sample;
 
-//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の偶数のみカンマ区切りで、[11]~[19]の値は奇数をカンマ区切りで表示させる
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
 public class Practice9 {
 	public static void main(String[] args) {
 		//要素数20の配列を宣言し
@@ -8,22 +8,22 @@ public class Practice9 {
 		for (int i = 0; i < num.length; i++) {
 			num[i] = i * 5;
 		}
-		//[0]~[10]の値の偶数のみカンマ区切り
-		System.out.println("[0]~[10]の値の偶数");
-		for (int i = 0; i <= 10; i++) {
-			if (i % 2 == 0) {
+		//[0]~[10]の値の奇数のみカンマ区切り
+		System.out.println("[0]~[10]の偶数");
+		for (int i = 1; i < 10; i++) {
+			if (i % 2 != 0) {
 				System.out.print(num[i]);
-				if (i < 10) {
+				if (i < 9) {
 					System.out.print(", ");
 				}
 			}
 		}
-		//[11]~[19]の値は奇数をカンマ区切り
-		System.out.println("\n[11]~[19]の奇数");
+		//[11]~[19]の値は偶数をカンマ区切り
+		System.out.println("\n[11]~[19]の偶数");
 		for (int i = 11; i < num.length; i++) {
-			if (i % 2 != 0) {
+			if (i % 2 == 0) {
 				System.out.print(num[i]);
-				if (i < 19) {
+				if (i < 18) {
 					System.out.print(", ");
 				}
 			}
