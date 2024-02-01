5d8f0c7fda4cd6ffbc6df3a019d680d7f5f3467e
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2024-02-02 00:02:47 +0900

    カンマを最後に表示させない方法

--- src/sample/Practice9.java
+++ src/sample/Practice9.java
@@ -10,21 +10,27 @@ public class Practice9 {
 		}
 		//[0]~[10]の値の奇数のみカンマ区切り
 		System.out.println("[0]~[10]の偶数");
+		boolean flag = true;
 		for (int i = 1; i < 10; i++) {
 			if (i % 2 != 0) {
-				System.out.print(num[i]);
-				if (i < 9) {
-					System.out.print(", ");
+				if (flag) {
+					System.out.print(num[i]);
+					flag = false;
+				} else {
+				System.out.print(", " + num[i]);
 				}
 			}
 		}
 		//[11]~[19]の値は偶数をカンマ区切り
 		System.out.println("\n[11]~[19]の偶数");
+		boolean flag2 = true;
 		for (int i = 11; i < num.length; i++) {
 			if (i % 2 == 0) {
-				System.out.print(num[i]);
-				if (i < 18) {
-					System.out.print(", ");
+				if (flag2) {
+					System.out.print(num[i]);
+					flag2 = false;
+				} else {
+				System.out.print(", " + num[i]);
 				}
 			}
 		}
